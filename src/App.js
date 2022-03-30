d3820d0877834567e975b65018e58f4024cca88f
Mayur <bhavsarmayur82@gmail.com>
2022-03-30 14:56:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import SearchMain from "./searchMain";
+
+function App() {
+  return (
+    <div className="App">
+      <SearchMain />
+    </div>
+  );
+}
+
+export default App;
